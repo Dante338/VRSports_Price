267df6591e2df5bb4a9972a8ca813ef0d46f220e
Dante338 <127452125+Dante338@users.noreply.github.com>
2025-10-07 15:29:22 -0500

    Da Game

---
+++ Assets/Scripts/VRSetup.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VRSetup : MonoBehaviour
+{
+    public XRController leftController;
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+}
